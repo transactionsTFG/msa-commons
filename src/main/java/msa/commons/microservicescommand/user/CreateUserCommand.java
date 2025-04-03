5b6179e88d62bbd7e50e9202ee584c7c6415889c
josevillacres46 <jvillacr@ucm.es>
2025-04-04 00:18:04 +0200

    FIX: All args const

--- src/main/java/msa/commons/microservicescommand/user/CreateUserCommand.java
+++ src/main/java/msa/commons/microservicescommand/user/CreateUserCommand.java
@@ -1,8 +1,10 @@
 package msa.commons.microservicescommand.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CreateUserCommand {
     private long idUser;
     private String typeUser;
