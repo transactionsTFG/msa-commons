5c8d092e93fd32fb1abddaf68fa551d8ea825fb5
josevillacres46 <jvillacr@ucm.es>
2025-04-05 19:29:09 +0200

    FIX: Rename property Weblogic error

--- src/main/java/msa/commons/consts/PropertiesConsumer.java
+++ src/main/java/msa/commons/consts/PropertiesConsumer.java
@@ -2,5 +2,5 @@ package msa.commons.consts;
 
 public class PropertiesConsumer {
     private PropertiesConsumer() {}
-    public static final String ORIGIN_QUEUE = "origin.queue";
+    public static final String ORIGIN_QUEUE = "origin_queue";
 }
