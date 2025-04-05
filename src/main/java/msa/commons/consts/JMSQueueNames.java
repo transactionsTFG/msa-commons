dd2d2f19ca348a159f01171c6a927f79c4e93b5e
josevillacres46 <jvillacr@ucm.es>
2025-04-05 20:26:46 +0200

    FEAT: Add jms country name in JMSQueueNames

--- src/main/java/msa/commons/consts/JMSQueueNames.java
+++ src/main/java/msa/commons/consts/JMSQueueNames.java
@@ -6,4 +6,5 @@ public class JMSQueueNames {
     public static final String AGENCY_USER_SERVICE_QUEUE = "jms/userServiceQueue";
     public static final String AGENCY_ORCHESTATOR_QUEUE = "jms/orchestatorQueue";
     public static final String AGENCY_TYPE_USER_SERVICE_QUEUE = "jms/typeUserServiceQueue";
+    public static final String AGENCY_COUNTRY_SERVICE_QUEUE = "jms/countryQueue";
 }
