e6eb1f9cfbb8f50846884d28c7309c1f7229f305
josevillacres46 <jvillacr@ucm.es>
2025-04-05 22:37:59 +0200

    FEAT: Add EventValue

--- src/main/java/msa/commons/event/EventValue.java
+++ src/main/java/msa/commons/event/EventValue.java
@@ -1,10 +1,30 @@
 package msa.commons.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class EventValue {
     private boolean success;
     private String message;
     private Object data;
+
+    public static EventValue success(Object data) {
+        return EventValue.builder()
+                .success(true)
+                .message("Success")
+                .data(data)
+                .build();
+    }
+
+    public static EventValue error(String message) {
+        return EventValue.builder()
+                .success(false)
+                .message(message)
+                .data(null)
+                .build();
+    }
 }
