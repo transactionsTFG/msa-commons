27fa8bdc4d7b3d9b261fad420c24e565fc3480a3
josevillacres46 <jvillacr@ucm.es>
2025-04-05 18:40:04 +0200

    FEAT: Add class consts

---
+++ src/main/java/msa/commons/consts/JMSQueueNames.java
@@ -0,0 +1,9 @@
+package msa.commons.consts;
+
+public class JMSQueueNames {
+    private JMSQueueNames() {}   
+    public static final String AGENCY_MONITORING_ERROR_QUEUE = "jms/monitoringErrorQueue";
+    public static final String AGENCY_USER_SERVICE_QUEUE = "jms/userServiceQueue";
+    public static final String AGENCY_ORCHESTATOR_QUEUE = "jms/orchestatorQueue";
+    public static final String AGENCY_TYPE_USER_SERVICE_QUEUE = "jms/typeUserServiceQueue";
+}
