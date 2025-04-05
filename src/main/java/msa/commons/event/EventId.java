fb55d8684ec734ad1db4aa813ed6e51f3e8d4493
josevillacres46 <jvillacr@ucm.es>
2025-04-05 22:39:14 +0200

    REFACTOR: Change get type for validate type user

--- src/main/java/msa/commons/event/EventId.java
+++ src/main/java/msa/commons/event/EventId.java
@@ -2,7 +2,7 @@ package msa.commons.event;
 
 public enum EventId {
     /* MICROSERVICIO DE TIPO USUARIO */
-    GET_TYPE_USER,
+    VALIDATE_TYPE_USER,
     /* MICROSERVICIO DE USUARIO */
     CREATE_USER,
     FAILED_USER,
