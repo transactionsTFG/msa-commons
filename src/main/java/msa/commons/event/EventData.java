f735ff6cf76765d4a5d9184b151ca2f2f4b39b4e
josevillacres46 <jvillacr@ucm.es>
2025-04-13 12:39:09 +0200

    FEAT: Add event Rollback

--- src/main/java/msa/commons/event/EventData.java
+++ src/main/java/msa/commons/event/EventData.java
@@ -1,9 +1,11 @@
 package msa.commons.event;
 
+import java.util.List;
+
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,12 +15,15 @@ import msa.commons.utils.GsonUtils;
 @AllArgsConstructor
 public class EventData {
     private String sagaId;
+    private List<EventId> rollbackEventTo;
     private Object data;
 
     public static <T> EventData fromJson(String json, Class<T> cast) {
         JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        final Gson gsonUtil = GsonUtils.getInstance();
         String sagaId = jsonObject.get("sagaId").getAsString();
-        T data = GsonUtils.getInstance().fromJson(jsonObject.get("data"), cast);   
-        return new EventData(sagaId, data);
+        List<EventId> rollbackEventTo = gsonUtil.fromJson(jsonObject.get("rollbackEventTo"), new TypeToken<List<EventId>>() {}.getType());
+        T data = gsonUtil.fromJson(jsonObject.get("data"), cast);   
+        return new EventData(sagaId, rollbackEventTo, data);
     }
 }
