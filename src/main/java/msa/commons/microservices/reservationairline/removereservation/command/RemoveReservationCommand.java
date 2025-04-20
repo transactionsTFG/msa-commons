13d2b15959843a55567c850b6a1515c5ff96389e
josevillacres46 <jvillacr@ucm.es>
2025-04-20 14:16:56 +0200

    FEAT: Add command

---
+++ src/main/java/msa/commons/microservices/reservationairline/removereservation/command/RemoveReservationCommand.java
@@ -0,0 +1,11 @@
+package msa.commons.microservices.reservationairline.removereservation.command;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class RemoveReservationCommand {
+    private Long idReservation;
+    private List<Long> listIdFlightInstance;
+}
