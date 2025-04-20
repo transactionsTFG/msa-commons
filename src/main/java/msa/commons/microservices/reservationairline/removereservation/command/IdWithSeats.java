a64ee44f84397ccd568d03c7addeeb1d68b93678
josevillacres46 <jvillacr@ucm.es>
2025-04-20 15:02:56 +0200

    FEAT: Add Loombok in class

--- src/main/java/msa/commons/microservices/reservationairline/removereservation/command/IdWithSeats.java
+++ src/main/java/msa/commons/microservices/reservationairline/removereservation/command/IdWithSeats.java
@@ -1,5 +1,12 @@
 package msa.commons.microservices.reservationairline.removereservation.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IdWithSeats {
     private Long idFlightInstance;
     private int seats;   
