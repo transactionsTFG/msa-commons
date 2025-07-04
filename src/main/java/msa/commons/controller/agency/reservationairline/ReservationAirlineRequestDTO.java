bea8ad6e10d4888ee583da97a9f98d42a4d86af4
josevillacres46 <jvillacr@ucm.es>
2025-07-04 20:33:24 +0200

    FEAT: Add DTO

---
+++ src/main/java/msa/commons/controller/agency/reservationairline/ReservationAirlineRequestDTO.java
@@ -0,0 +1,13 @@
+package msa.commons.controller.agency.reservationairline;
+
+import java.util.List;
+
+import lombok.Data;
+import msa.commons.controller.airline.reservation.create.FlightInstanceSeatsDTO;
+
+@Data
+public class ReservationAirlineRequestDTO {
+    private String dni;
+    private long idCustomer;
+    private List<FlightInstanceSeatsDTO> listIdFlightInstanceSeats;
+}
