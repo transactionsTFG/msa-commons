f66e22e58e4fa551dd34498534abba10da0b2801
josevillacres46 <jvillacr@ucm.es>
2025-07-11 23:01:50 +0200

    fEAT: Add remove Events Ids

--- src/main/java/msa/commons/event/eventoperation/reservation/ReservationAirline.java
+++ src/main/java/msa/commons/event/eventoperation/reservation/ReservationAirline.java
@@ -3,7 +3,7 @@ package msa.commons.event.eventoperation.reservation;
 import msa.commons.event.eventoperation.EventOperation;
 
 public enum ReservationAirline implements EventOperation {
-    CREATE_RESERVATION_ONLY_AIRLINE,
+    CREATE_RESERVATION_ONLY_AIRLINE_BEGIN,
     CREATE_RESERVATION_ONLY_AIRLINE_COMMIT,
     CREATE_RESERVATION_ONLY_AIRLINE_ROLLBACK;
 
