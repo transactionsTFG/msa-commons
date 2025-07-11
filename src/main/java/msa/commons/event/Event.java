860e9f6aea1f0304a998e8f143972459f8ad42c4
josevillacres46 <jvillacr@ucm.es>
2025-07-11 20:41:38 +0200

    FEAT: Move Operation in EventData

--- src/main/java/msa/commons/event/Event.java
+++ src/main/java/msa/commons/event/Event.java
@@ -2,17 +2,9 @@ package msa.commons.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import msa.commons.event.eventoperation.EventOperation;
-
 @Data
 @AllArgsConstructor
 public class Event {
     private EventId eventId;
     private EventData value;
-    private EventOperation operation;
-
-    public Event(EventId eventId, EventData value) {
-        this.eventId = eventId;
-        this.value = value;
-    }
 }
--- src/main/java/msa/commons/event/EventData.java
+++ src/main/java/msa/commons/event/EventData.java
@@ -9,21 +9,33 @@ import com.google.gson.reflect.TypeToken;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import msa.commons.event.eventoperation.EventOperation;
 import msa.commons.utils.GsonUtils;
 
 @Data
 @AllArgsConstructor
 public class EventData {
     private String sagaId;
+    private EventOperation operation;
     private List<EventId> rollbackEventTo;
     private Object data;
+
+    public EventData(String sagaId, List<EventId> rollbackEventTo, Object data) {
+        this.sagaId = sagaId;
+        this.rollbackEventTo = rollbackEventTo;
+        this.data = data;
+    }
 
     public static <T> EventData fromJson(String json, Class<T> cast) {
         JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
         final Gson gsonUtil = GsonUtils.getInstance();
         String sagaId = jsonObject.get("sagaId").getAsString();
         List<EventId> rollbackEventTo = gsonUtil.fromJson(jsonObject.get("rollbackEventTo"), new TypeToken<List<EventId>>() {}.getType());
-        T data = gsonUtil.fromJson(jsonObject.get("data"), cast);   
+        T data = gsonUtil.fromJson(jsonObject.get("data"), cast);       if(jsonObject.get("operation") != null) {
+            EventOperation operation = gsonUtil.fromJson(jsonObject.get("operation"), EventOperation.class);
+            return new EventData(sagaId, operation, rollbackEventTo, data);
+        }
         return new EventData(sagaId, rollbackEventTo, data);
+ 
     }
 }
