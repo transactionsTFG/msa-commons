64da42c5433bc7f934d8b6faa69906fd685dea12
josevillacres46 <jvillacr@ucm.es>
2025-07-19 14:30:09 +0200

    FIX: More errors

--- src/main/java/msa/commons/consts/JMSQueueNames.java
+++ src/main/java/msa/commons/consts/JMSQueueNames.java
@@ -4,8 +4,8 @@ public class JMSQueueNames {
     private JMSQueueNames() {}   
     /* JNDI ConnectionFactory */
     public static final String CONNECTION_FACTORY_LOCAL = "jms/connectionFactory";
-    public static final String CONNECTION_FACTORY_AGENCY = "jmsremote/connectionFactoryAgency";
-    public static final String CONNECTION_FACTORY_AIRLINE = "jmsremote/connectionFactoryAirline";
+    public static final String CONNECTION_FACTORY_AGENCY = "jms/remoteConnectionFactoryAgency";
+    public static final String CONNECTION_FACTORY_AIRLINE = "jms/remoteConnectionFactoryAirline";
     /* JNDI AGENCY */
     public static final String AGENCY_MONITORING_ERROR_QUEUE = "jms/agencyMonitoringErrorQueue";
     public static final String AGENCY_USER_SERVICE_QUEUE = "jms/userServiceQueue";
@@ -13,8 +13,8 @@ public class JMSQueueNames {
     public static final String AGENCY_TYPE_USER_SERVICE_QUEUE = "jms/typeUserServiceQueue";
     public static final String AGENCY_COUNTRY_SERVICE_QUEUE = "jms/countryQueue";
     public static final String AGENCY_TRAVEL_SERVICE_QUEUE = "jms/travelQueue";
-    public static final String ORCHESTRATOR_AIRLINE_QUEUE = "jmsremote/orchestratorAirline";
-    
+    public static final String REMOTE_ORCHESTRATOR_AIRLINE_QUEUE = "jms/remoteOrchestratorAirline";
+
     /* JNDI HOTEL */
     public static final String AGENCY_HOTEL_BOOKING_SERVICE_QUEUE = "jms/hotelBookingServiceQueue";
     public static final String AGENCY_HOTEL_ROOM_SERVICE_QUEUE = "jms/hotelRoomServiceQueue";
@@ -30,5 +30,5 @@ public class JMSQueueNames {
     public static final String AIRLINE_CUSTOMER = "jms/customerQueue";
     public static final String AIRLINE_ORCHESTATOR_QUEUE = "jms/airlineOrchestatorQueue";
     public static final String AIRLINE_RESERVATION_QUEUE = "jms/reservationQueue";
-    public static final String REMOTE_ORCHESTRATOR_AGENCY_QUEUE = "jmsremote/orchestratorAgency";
+    public static final String REMOTE_ORCHESTRATOR_AGENCY_QUEUE = "jms/remoteOrchestratorAgency";
 }
