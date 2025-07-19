3c6bfb6376d2c7a9721c0474b1ff57a54b1243ce
josevillacres46 <jvillacr@ucm.es>
2025-07-19 18:48:45 +0200

    FEAT: Add properties

--- src/main/java/msa/commons/consts/JMSQueueNames.java
+++ src/main/java/msa/commons/consts/JMSQueueNames.java
@@ -6,6 +6,8 @@ public class JMSQueueNames {
     public static final String CONNECTION_FACTORY_LOCAL = "jms/connectionFactory";
     public static final String CONNECTION_FACTORY_AGENCY = "jms/remoteConnectionFactoryAgency";
     public static final String CONNECTION_FACTORY_AIRLINE = "jms/remoteConnectionFactoryAirline";
+    public static final String CONNECTION_FACTORY_HOTEL = "jms/remoteConnectionFactoryHotel";
+    
     /* JNDI AGENCY */
     public static final String AGENCY_MONITORING_ERROR_QUEUE = "jms/agencyMonitoringErrorQueue";
     public static final String AGENCY_USER_SERVICE_QUEUE = "jms/userServiceQueue";
@@ -14,14 +16,15 @@ public class JMSQueueNames {
     public static final String AGENCY_COUNTRY_SERVICE_QUEUE = "jms/countryQueue";
     public static final String AGENCY_TRAVEL_SERVICE_QUEUE = "jms/travelQueue";
     public static final String REMOTE_ORCHESTRATOR_AIRLINE_QUEUE = "jms/remoteOrchestratorAirline";
+    public static final String REMOTE_ORCHESTRATOR_HOTEL_QUEUE = "jms/remoteOrchestratorHotel";
+    public static final String REMOTE_ORCHESTRATOR_AGENCY_QUEUE = "jms/remoteOrchestratorAgency";
 
     /* JNDI HOTEL */
-    public static final String AGENCY_HOTEL_BOOKING_SERVICE_QUEUE = "jms/hotelBookingServiceQueue";
-    public static final String AGENCY_HOTEL_ROOM_SERVICE_QUEUE = "jms/hotelRoomServiceQueue";
     public static final String HOTEL_ORCHESTRATOR_QUEUE = "jms/hotelOrchestatorQueue";
     public static final String HOTEL_BOOKING_QUEUE = "jms/hotelBookingQueue";
     public static final String HOTEL_CUSTOMER_QUEUE = "jms/hotelCustomerQueue";
     public static final String HOTEL_ROOM_QUEUE = "jms/hotelRoomQueue";
+    
     /* JNDI AIRLINE */
     public static final String AIRLINE_MONITORING_ERROR_QUEUE = "jms/airlineMonitoringErrorQueue";
     public static final String AIRLINE_AIRCRAFT_QUEUE = "jms/aircraftQueue";
@@ -30,5 +33,4 @@ public class JMSQueueNames {
     public static final String AIRLINE_CUSTOMER = "jms/customerQueue";
     public static final String AIRLINE_ORCHESTATOR_QUEUE = "jms/airlineOrchestatorQueue";
     public static final String AIRLINE_RESERVATION_QUEUE = "jms/reservationQueue";
-    public static final String REMOTE_ORCHESTRATOR_AGENCY_QUEUE = "jms/remoteOrchestratorAgency";
 }
